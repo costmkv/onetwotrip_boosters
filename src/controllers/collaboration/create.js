6beeb8393baed82b5ac8a8cd69620cd9ce982086
Constantine Makeev <constmkv@gmail.com>
2019-09-17 14:54:19 +0300

    remove excess data

--- src/controllers/collaboration/create.js
+++ src/controllers/collaboration/create.js
@@ -5,31 +5,6 @@ const getTestMessage = () => ({ message: 'Hello Boosters.pro' });
 
 const getExtendedTicket = async (models, config, ticket) => {
 	try {
-		ticket = {
-			"from": {
-				"code": 2000001,
-				"station": "Москва Курская",
-				"metaId": 22823,
-				"metaName": "Москва",
-				"initCode": 2060001,
-				"initDate": "2019-09-14T00:00:00"
-			},
-			"to": {
-				"code": 2004001,
-				"station": "Санкт-Петербург-Главн.",
-				"metaId": 22871,
-				"metaName": "Санкт-Петербург",
-				"finalCode": 2004001
-			},
-			"departure": {
-				"time": "2019-09-15T00:44:00",
-				"localTime": "2019-09-15T00:44:00",
-				"local": false,
-				"utcTimeOffset": 3,
-				"stopInMinutes": 15
-			},
-		};
-
 		const existingTicket = await getTicket(models, ticket);
 
 		let result;
